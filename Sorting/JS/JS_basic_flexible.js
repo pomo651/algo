49d84d04a7c3ba74360d9f971861bb1787a033d1
catherine <cathyweiyiliu@gmail.com>
2020-10-27 20:43:07 -0700

    edited regarding review

--- Sorting/JS/JS_basic_flexible.js
+++ Sorting/JS/JS_basic_flexible.js
@@ -1,26 +1,21 @@
+// flexible & scalable rather than hard coding
+// 尽量让代码可以复用，不受个别例子影响
 
+// 题目var a = ["Wind", "Water", "Fire"];  如何打印出 "Wind, Water, Fire" ?
 
-flexible & scalable rather than hard coding
-尽量让代码可以复用，不受个别例子影响
+var a = ["Wind", "Water", "Fire"];
+const delimiter = ", ";
+console.log(a.join(delimiter));
 
-var a = ['Wind', 'Water', 'Fire'];  如何打印出 'Wind, Water, Fire' ?
+// join 的基本用法
+// The join() method creates and returns a new string. If an element is undefined, null or an empty array [], it is converted to an empty string.
 
-var a = ['Wind', 'Water', 'Fire'];
-const delimiter = ‘, ';
-console.log(a.join(delimiter));  
-
-
-join 的基本用法
-The join() method creates and returns a new string. If an element is undefined, null or an empty array [], it is converted to an empty string.
-
-const elements = ['Fire', 'Air', 'Water'];
-
+const elements = ["Fire", "Air", "Water"];
 console.log(elements.join());
 // expected output: ‘Fire,Air,Water’
 
-var a = ['Wind', 'Water', 'Fire'];
-a.join();      // 'Wind,Water,Fire'
-a.join(', ');  // 'Wind, Water, Fire'
-a.join(' + '); // 'Wind + Water + Fire'
-a.join('');    // 'WindWaterFire'
-
+var a = ["Wind", "Water", "Fire"];
+a.join(); // 'Wind,Water,Fire'
+a.join(", "); // 'Wind, Water, Fire'
+a.join(" + "); // 'Wind + Water + Fire'
+a.join(""); // 'WindWaterFire'
